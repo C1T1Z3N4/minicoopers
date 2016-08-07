1b5467c436b62ac0b212c60cbe11cf7bdb4455f9
joe@wealthkernel.com <joecampbell1974@gmail.com>
2016-08-07 01:32:33 +0100

    Introduced clans json object.  Scores table generated automatically

---
+++ app.js
@@ -0,0 +1,52 @@
+var descendingScore = function(a, b) {
+    return b.score - a.score;
+};
+
+var descendingTotal = function(a, b) {
+    return b.total - a.total;
+};
+
+var calculateTotals = function() {
+    $.each(clans, function(i, clan) {
+        var total = 0;
+        $.each(clan.members, function(j, member) {
+            total += member.score;
+        });
+        clan.total = total;
+
+        clan.members = clan.members.sort(descendingScore);
+    });
+
+    clans = clans.sort(descendingTotal);
+};
+
+var generateScores = function() {
+
+    var scoresElement = $('#scores');
+
+    $.each(clans, function(i, clan) {
+
+        var rowspan = clan.members.length;
+
+        $.each(clan.members, function(j, member) {
+
+            var row = null;
+
+            if (j == 0 ) {
+                var row = $('<tr><td rowspan="' + rowspan + '">The ' + clan.name + '</td><td>' + member.name + '</td><td>' + member.score + '</td><td rowspan="' + rowspan + '">' + clan.total + '</td></tr>');
+
+            } else {
+                var row = $('<tr><td>' + member.name + '</td><td>' + member.score + '</td></tr>');
+            }
+
+            scoresElement.append(row);
+        });
+    });
+};
+
+var bootstrapper = function() {
+    calculateTotals();
+    generateScores();
+}
+
+$(document).ready(bootstrapper);
---
+++ clans.js
@@ -0,0 +1,48 @@
+var clans = [{
+    "name": "Bruces",
+    "members": [{
+      "name": "Anna",
+      "score": 0
+    }, {
+      "name": "Caleb",
+      "score": 2
+    }, {
+      "name": "Caroline",
+      "score": 0
+    }, {
+      "name": "Ethan",
+      "score": 2
+    }, {
+      "name": "Pete",
+      "score": 15
+    }]
+  },
+
+  {
+    "name": "Campbells",
+    "members": [{
+      "name": "Callum",
+      "score": 7
+    }, {
+      "name": "Joe",
+      "score": 2
+    }, {
+      "name": "Malachy",
+      "score": 4
+    }]
+  },
+
+  {
+    "name": "Kerrs",
+    "members": [{
+      "name": "James",
+      "score": 0
+    }, {
+      "name": "Peter",
+      "score": 10
+    }, {
+      "name": "Sarah",
+      "score": 0
+    }]
+  }
+];
