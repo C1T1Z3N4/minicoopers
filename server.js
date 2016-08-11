6a79fd8b4c861d0ba4a2f76ffb6ecbcf74f6ec12
joe@wealthkernel.com <joecampbell1974@gmail.com>
2016-08-11 18:29:33 +0100

    Reading data from Mongo

--- server.js
+++ server.js
@@ -6,69 +6,31 @@
     var log         = require('winston');
     var logger    = require('morgan');
     var path       = require('path');
+    var mongoose = require('mongoose');
 
     var rootDir = path.dirname(process.mainModule.filename);
     var publicDir = path.join(rootDir, 'public');
 
-    log.info('publicDir ' + publicDir);
-
     app.use(logger('combined'));
     app.use(express.static(publicDir));
 
-    app.get("/clans", function(req, res) {
-var clans = [{
-    "name": "Bruces",
-    "members": [{
-      "name": "Anna",
-      "score": 3
-    }, {
-      "name": "Caleb",
-      "score": 4
-    }, {
-      "name": "Caroline",
-      "score": 2
-    }, {
-      "name": "Ethan",
-      "score": 3
-    }, {
-      "name": "Pete",
-      "score": 26
-    }]
-  },
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/minicoopers');
 
-  {
-    "name": "Campbells",
-    "members": [{
-      "name": "Callum",
-      "score": 9
-    }, {
-      "name": "Joe",
-      "score": 5
-    }, {
-      "name": "Malachy",
-      "score": 4
-    }]
-  },
+    var Schema = mongoose.Schema;
 
-  {
-    "name": "Kerrs",
-    "members": [{
-      "name": "James",
-      "score": 2
-    }, {
-      "name": "Peter",
-      "score": 10
-    }, {
-      "name": "Sarah",
-      "score": 2
-    }]
-  }
-];
-
-res.json(clans);
+    var clanSchema = new Schema({
+      name: String
     });
 
-     var port = process.env.PORT || 5000;
+    var Clans = mongoose.model('Clans', clanSchema);
+
+    app.get("/clans", function(req, res) {
+        Clans.find({}, function(err, clans) {
+            res.send(clans);
+        })
+    });
+
+    var port = process.env.PORT || 5000;
     app.listen(port, function() {
         log.info("Mini cooper counts started on " + port);
     });
