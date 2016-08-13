2a1e7120c3b50754c0110a44685cf2adebe6332b
joe@wealthkernel.com <joecampbell1974@gmail.com>
2016-08-13 18:00:08 +0100

    meh

--- server.js
+++ server.js
@@ -14,7 +14,7 @@
 
     app.use(logger('combined'));
     app.use(express.static(publicDir));
-    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.get("/api/clans", function(req, res) {
         clans.all(function(err, clans) {
